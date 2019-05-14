d320a8f83d0a5ed0746fb1813101b289cc773372
Moses <ogbopinamoses@gmail.com>
2019-05-14 18:07:41 +0100

    Modified settings.py

--- tmdb/settings.py
+++ tmdb/settings.py
@@ -133,4 +133,4 @@ STATICFILES_DIRS = (
 )
 
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
